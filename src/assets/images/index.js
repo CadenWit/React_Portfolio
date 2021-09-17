2adfaffa34b6c541e68ce2c543c6549493a8d8dd
CadenWit <cadenwitbeck@gmail.com>
2021-09-16 20:16:38 -0400

    Updates

--- src/assets/images/index.js
+++
@@ -1,9 +0,0 @@
-const IMAGES = {
-  movieSnobs: require("./movie-snob.png").default,
-  noteTaker: require("./NoteTaker.png").default,
-  workDayScheduler: require("./Work-Day-Scheduler.png").default,
-  avatar: require("./Avatar.png").default,
-  resume: require("./Resume.png").default,
-};
-
-export default IMAGES;
